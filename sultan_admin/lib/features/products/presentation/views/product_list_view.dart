210cdfe43d6fb0a9727336077efdfe7e37b3087b
Attaf Massil <massilattaf04@gmail.com>
2025-11-21 04:24:54 +0100

    adding ProductCard

--- sultan_admin/lib/features/products/presentation/views/product_list_view.dart
+++ sultan_admin/lib/features/products/presentation/views/product_list_view.dart
@@ -5,6 +5,7 @@ import 'package:sultan_admin/features/products/data/models/product_model.dart';
 import 'package:sultan_admin/features/products/presentation/view_models/product_view_model.dart';
 import 'package:sultan_admin/features/products/presentation/views/add_edit_product_view.dart';
 import 'package:sultan_admin/features/products/presentation/views/product_details_view.dart';
+import 'package:sultan_admin/features/products/presentation/widgets/product_item.dart';
 import 'package:sultan_admin/shared/components/app_bar.dart';
 import 'package:sultan_admin/shared/components/search_field.dart';
 import 'package:sultan_admin/shared/components/image.dart';
@@ -130,7 +131,7 @@ class _ProductListViewState extends ConsumerState<ProductListView> {
                   itemCount: products.length,
                   itemBuilder: (context, index) {
                     final product = products[index];
-                    return _ProductCard(product: product);
+                    return ProductCard(product: product);
                   },
                 );
               },
@@ -139,99 +140,6 @@ class _ProductListViewState extends ConsumerState<ProductListView> {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class _ProductCard extends ConsumerWidget {
-  final Product product;
-
-  const _ProductCard({required this.product});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => ProductDetailsView(product: product),
-          ),
-        );
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          color: context.c_surface,
-          borderRadius: BorderRadius.circular(12.r),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 8,
-              offset: const Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: ReusableImage(
-                imageUrl: product.imageUrl,
-                width: double.infinity,
-                height: double.infinity,
-                borderRadius: 12.r,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(12.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 4.h),
-                  Text(
-                    '\$${product.price}',
-                    style: context.body?.copyWith(
-                      color: context.c_primary,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 8.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) =>
-                                  AddEditProductView(product: product),
-                            ),
-                          );
-                        },
-                        child: Icon(Icons.edit, size: 20.r, color: Colors.blue),
-                      ),
-                      SizedBox(width: 8.w),
-                      InkWell(
-                        onTap: () {
-                          ref
-                              .read(productViewModelProvider.notifier)
-                              .deleteProduct(product.id);
-                        },
-                        child: Icon(
-                          Icons.delete,
-                          size: 20.r,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
---
+++ sultan_admin/lib/features/products/presentation/widgets/product_item.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sultan_admin/features/products/data/models/product_model.dart';
+import 'package:sultan_admin/features/products/presentation/view_models/product_view_model.dart';
+import 'package:sultan_admin/features/products/presentation/views/add_edit_product_view.dart';
+import 'package:sultan_admin/features/products/presentation/views/product_details_view.dart';
+import 'package:sultan_admin/shared/components/image.dart';
+import 'package:sultan_admin/utils/extensions/color_extension.dart';
+import 'package:sultan_admin/utils/extensions/text_extension.dart';
+
+class ProductCard extends ConsumerWidget {
+  final Product product;
+
+  const ProductCard({required this.product});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => ProductDetailsView(product: product),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: context.c_surface,
+          borderRadius: BorderRadius.circular(12.r),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ReusableImage(
+                imageUrl: product.imageUrl,
+                width: double.infinity,
+                height: double.infinity,
+                borderRadius: 12.r,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(12.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 4.h),
+                  Text(
+                    '\$${product.price}',
+                    style: context.body?.copyWith(
+                      color: context.c_primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) =>
+                                  AddEditProductView(product: product),
+                            ),
+                          );
+                        },
+                        child: Icon(Icons.edit, size: 20.r, color: Colors.blue),
+                      ),
+                      SizedBox(width: 8.w),
+                      InkWell(
+                        onTap: () {
+                          ref
+                              .read(productViewModelProvider.notifier)
+                              .deleteProduct(product.id);
+                        },
+                        child: Icon(
+                          Icons.delete,
+                          size: 20.r,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
